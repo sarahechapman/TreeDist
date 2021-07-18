b475309854685882a1ea988001b660a326692b83
Sarah Chapman <sarahechapman@hotmail.com>
2021-07-18 13:08:35 +0100

    Create tree_distance_hvi.R

---
+++ R/tree_distance_hvi.R
@@ -0,0 +1,14 @@
+#' Hierarchical Variation of Information distance
+#' 
+#' Calculate the hierachicical variation of information distance
+#' 
+#' Explain here how the hierachical variation of information distance works
+#' @export
+HierachicalVariation <- function (tree1, tree2 = NULL) {
+  library(reticulate)
+  cat(tree1)
+  cat(tree2)
+  source_python("hit.py")
+  VariationInformation <- HVI(tree1,tree2)
+  VariationInformation
+}
