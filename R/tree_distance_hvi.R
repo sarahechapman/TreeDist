f0e6276f7c71ac59f2fc5d416136b07eec7a6277
Sarah Chapman <sarahechapman@hotmail.com>
2021-07-22 01:33:48 +0100

    Update tree_distance_hvi.R

--- R/tree_distance_hvi.R
+++ R/tree_distance_hvi.R
@@ -6,10 +6,11 @@
 #' @export
 HierachicalVariation <- function (tree1, tree2 = NULL) {
   library(reticulate)
-  setwd("..")
-  source_python("hit.py")
-  VariationInformation <- HVI(tree1,tree2)
+  setwd("~")
+  source_python("documents/GitHub/TreeDist/R/hit.py")
+  treeA <- ape::write.tree(tree1)
+  treeB <- ape::write.tree(tree2)
+  VariationInformation <- HVI(treeA, treeB)
   VariationInformation
   setwd("~")
 }
-
